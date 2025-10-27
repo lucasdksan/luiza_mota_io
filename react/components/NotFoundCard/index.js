2cece97a386ee3e23d468533afe2794adca4579c
lucas <lucas.leoncio.silva@gmail.com>
2025-10-27 01:06:15 -0300

    feat/department - fim do conteudo simples da página not found

--- react/components/NotFoundCard/index.js
+++ react/components/NotFoundCard/index.js
@@ -1,3 +1,7 @@
+import React from "react";
+import { schema } from "./schema";
+import { classGenerator } from "../../utils/classGenerator";
+
 function NotFoundCard({
     VtexFlexLayout,
     checkList,
@@ -5,11 +9,16 @@ function NotFoundCard({
 }) {
     return (
         <VtexFlexLayout>
-            <div>
-                <h2>{titleRedCard}</h2>
-                <ul>
+            <div className={classGenerator("vtex-not-found-card", "container")}>
+                <h2 className={classGenerator("vtex-not-found-card", "title")}>{titleRedCard}</h2>
+                <ul className={classGenerator("vtex-not-found-card", "list")}>
                     {checkList.map((item, index) => (
-                        <li key={index}>{item.title}</li>
+                        <li className={classGenerator("vtex-not-found-card", "item")} key={index}>
+                            <svg className={classGenerator("vtex-not-found-card", "item-icon")} xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
+                                <path d="M2 6.30556L4.46154 8.75L10 3.25" stroke="#484849" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                            <span className={classGenerator("vtex-not-found-card", "item-text")}>{item.title}</span>
+                        </li>
                     ))}
                 </ul>
             </div>
