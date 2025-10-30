be43a7447b8b1d2cccefc4bf0f116179b35a01c7
lucas <lucas.leoncio.silva@gmail.com>
2025-10-30 00:58:42 -0300

    Construção da descrição do produto

--- react/components/ProductDefinitions/index.js
+++ react/components/ProductDefinitions/index.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { useProduct } from "vtex.product-context";
 
 function ProductDefinitions({
     VtexFlexLayout
 }) {
-    const product = useProduct();
+    const [productDefinitions, setProductDefinitions] = useState([]);
+    const { product } = useProduct();
+    const { description, specificationGroups  } = product;
+
+    useEffect(() => {
+        if(description) {
+            setProductDefinitions((prev) => [...prev, {
+                title: "Descrição",
+                description: description
+            }]);
+        }
+
+        if(specificationGroups) {
+            
+        }
+    }, [description]);
 
     console.log("product: ", product);
 
