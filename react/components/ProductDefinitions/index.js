bc498b7cb625c8dcc0885317b30a45ba8263961d
lucas <lucas.leoncio.silva@gmail.com>
2025-10-30 10:36:29 -0300

    Criação do componente de descrição e informações gerais do produto

--- react/components/ProductDefinitions/index.js
+++ react/components/ProductDefinitions/index.js
@@ -1,34 +1,79 @@
 import React, { useEffect, useState } from "react";
-
 import { useProduct } from "vtex.product-context";
+import { classGenerator } from "../../utils/classGenerator";
 
-function ProductDefinitions({
-    VtexFlexLayout
-}) {
-    const [productDefinitions, setProductDefinitions] = useState([]);
-    const { product } = useProduct();
-    const { description, specificationGroups  } = product;
+function ProductDefinitions({ VtexFlexLayout }) {
+  const [productDefinitions, setProductDefinitions] = useState([]);
+  const [activeTab, setActiveTab] = useState("Descrição");
+  const { product } = useProduct();
+  const { description, specificationGroups } = product ?? {};
 
-    useEffect(() => {
-        if(description) {
-            setProductDefinitions((prev) => [...prev, {
-                title: "Descrição",
-                description: description
-            }]);
-        }
+  useEffect(() => {
+    const definitions = [];
 
-        if(specificationGroups) {
-            
-        }
-    }, [description]);
+    if (description) {
+      definitions.push({
+        title: "Descrição",
+        description: description,
+      });
+    }
 
-    console.log("product: ", product);
+    if (specificationGroups && specificationGroups.length > 0) {
+      const allSpecifications = specificationGroups.find(
+        (group) => group.name === "allSpecifications"
+      );
 
-    return (
-        <VtexFlexLayout>
-            
-        </VtexFlexLayout>
-    );
+      if (allSpecifications && allSpecifications.specifications?.length > 0) {
+        const html = allSpecifications.specifications
+          .map((spec) => {
+            const value = spec.values?.join("<br>") ?? "";
+            return `
+              <h3 style="margin: 0px;">${spec.name}</h3>
+              <p style="margin: 0px;">${value}</p>
+            `;
+          })
+          .join("");
+
+        definitions.push({
+          title: "Tamanhos e especificações",
+          description: html,
+        });
+      }
+    }
+
+    setProductDefinitions(definitions);
+  }, [description, specificationGroups]);
+
+  const activeContent = productDefinitions.find(
+    (item) => item.title === activeTab
+  );
+
+  return (
+    <VtexFlexLayout>
+      <div className={classGenerator("vtex-product-definitions", "container")}>
+        <div className={classGenerator("vtex-product-definitions", "tabs")}>
+          {productDefinitions.map((item) => (
+            <button
+              key={item.title}
+              onClick={() => setActiveTab(item.title)}
+              className={`${classGenerator("vtex-product-definitions", "tab")}  ${
+                activeTab === item.title
+                  ? classGenerator("vtex-product-definitions", "tab--active")
+                  : classGenerator("vtex-product-definitions", "tab--inactive")
+              }`}
+            >
+              {item.title}
+            </button>
+          ))}
+        </div>
+
+        <div
+          className={classGenerator("vtex-product-definitions", "content-description")}
+          dangerouslySetInnerHTML={{ __html: activeContent?.description || "" }}
+        />
+      </div>
+    </VtexFlexLayout>
+  );
 }
 
 export default ProductDefinitions;
